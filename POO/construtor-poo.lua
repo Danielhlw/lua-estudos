fa182d5ae1e9d88df545cdf3ff00d977a634db4b
Daniel <hollowdenis@gmail.com>
2025-02-25 08:14:52 -0300

    adicionando exemplo básico de 'classe'

---
+++ POO/construtor-poo.lua
@@ -0,0 +1,19 @@
+Pessoa = {}
+
+function Pessoa:new(nome, idade)
+    local obj = {nome = nome, idade = idade}
+    setmetatable(obj, self) 
+    self.__index = self
+    return obj
+end
+
+function Pessoa:dizer_ola()
+    print("Oi, eu sou " .. self.nome .. " e tenho " .. self.idade .. " anos!")
+end
+
+-- Criando Objetos
+local p1 = Pessoa:new("Daniel", 21)
+local p2 = Pessoa:new("Mariana", 29)
+
+p1:dizer_ola()
+p2:dizer_ola()
